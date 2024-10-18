e40ec6ee3a8531e71d7bf9bcd9da762f10ac3612
Gabriel Lane Gillette <gabedogman@gmail.com>
2024-10-18 16:25:14 -0400

    Added stub file for cpp source files.

---
+++ src/stub.cpp
@@ -0,0 +1,112 @@
+/**
+ * @file stub.cpp
+ * @brief  A stub source file.
+ * @authors  Gabriel Gillette
+ * @copyright  (c) 2024 Gabriel Gillette
+ * @date Last Modified: Oct 18, 2024
+ * @note See header for full documentation.
+ */
+
+/* MIT License
+
+Copyright (c) 2024 Gabriel Gillette
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. */
+
+
+/*-------------------------------------------------------- STANDARD INCLUDES */
+
+/*----------------------------------------------------------- LOCAL INCLUDES */
+#include "stub.h"
+
+/*----------------------------------------------------- THIRD PARTY INCLUDES */
+
+/*-------------------------------------------------------- MACRO DEFINITIONS */
+
+/**
+ @def STUB_MACRO
+*/
+// #define STUB_MACRO
+
+
+/*------------------------------------------------------- STRUCT DEFINITIONS */
+
+/**
+ @struct stub_struct
+*/
+// struct stub_struct {
+// } stub_struct;
+
+/*--------------------------------------------NAMESPACE VARIABLE DEFINITIONS */
+
+/**
+ @var stubNamespaceVariable
+*/
+// static stub::stubNamespaceVariable = 0;
+
+/*------------------------------------------- NAMESPACE FUNCTION DEFINITIONS */
+
+/**
+ * <A short one line description>
+ */
+//static void stub::StubFunc() {
+//    return;
+//} // StubFunc
+
+/*---------------------------------------------------- CLASS IMPLEMENTATIONS */
+
+/**
+ * @class Stub
+ * <A short one line description>
+ */
+
+/*-------------------------------------------------------------- CONSTRUCTOR */
+
+stub::Stub::Stub() = default;
+
+/*--------------------------------------------------------------- DESTRUCTOR */
+
+stub::Stub::~Stub() = default;
+
+/*--------------------------------------------------------- COPY CONSTRUCTOR */
+
+stub::Stub::Stub(const Stub& other) = default;
+
+/*-------------------------------------------------------------- COPY ASSIGN */
+
+stub::Stub& stub::Stub::operator=(const Stub& other) = default;
+
+/*--------------------------------------------------------- MOVE CONSTRUCTOR */
+
+stub::Stub::Stub(Stub&& other) noexcept = default;
+
+/*-------------------------------------------------------------- MOVE ASSIGN */
+
+stub::Stub& stub::Stub::operator=(Stub&& other) noexcept = default;
+
+/*---------------------------------------------------- PRIVATE CLASS METHODS */
+
+/**
+ * <A short one line description>
+ */
+void stub::Stub::StubMethod(){
+  return;
+} // StubMethod
+
+/*----------------------------------------------------- PUBLIC CLASS METHODS */
